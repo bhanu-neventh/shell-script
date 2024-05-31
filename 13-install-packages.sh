5bcb3be589ad2d5a99061a9dfc767a8e32a7ed1d
bhanu-neventh <bhanuneventh@gmail.com>
2024-05-31 16:29:25 +0530

    added 2nd script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -2,6 +2,26 @@
 
 ID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started execting at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ...... $R FAILED $N"
+    else
+        echo -e "$2 ...... $G SUCCESS $N"
+    fi
+}
+
+
 if [ $ID -ne 0 ]
 then 
     echo -e "$R ERROR :: Please run this script with root access $N"
@@ -10,7 +30,19 @@ else
     echo "you are root user"
 fi
 
-echo "All Arguments passed : $@"
+#echo "All Arguments passed : $@"
 
+for package in $@
 
+do 
+    yum list installed $package &>> $LOGFILE
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y &>> $LOGFILE
+        VALIDATE $? "INSTALLATION OF $package"
+    else
+        echo -e "$package is already installed ... $Y SKKIPPING $N"
+    fi
+done
 
+        
